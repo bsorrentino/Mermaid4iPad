894eb28bbae197bc7312274c9d39b02d9e602a14
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-07-27 23:00:20 +0200

    feat: complete mermaid-preview webcomponent

---
+++ MermaidPreview/web/clean.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env bun
+import { $ } from "bun";
+
+$.nothrow();
+await $`rm -r .parcel-cache`
+await $`rm -r dist`
---
+++ MermaidPreview/web/deploy.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env bun
+import { $ } from "bun";
+
+$.nothrow();
+await $`./clean.js`
+await $`bun run build`
+await $`rm ../Sources/MermaidPreview/_Resources/*`
+await $`cp dist/* ../Sources/MermaidPreview/_Resources`
