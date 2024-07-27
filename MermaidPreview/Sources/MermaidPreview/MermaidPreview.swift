c470d4a61747b34a3eb60cef07f9dab2fad1b39d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-07-27 23:16:22 +0200

    feat: add new MermaidPreview  package

--- MermaidPreview/Sources/MermaidPreview/MermaidPreview.swift
+++ MermaidPreview/Sources/MermaidPreview/MermaidPreview.swift
@@ -7,14 +7,23 @@ import SwiftUI
 
 public struct MermaidPreview : UIViewControllerRepresentable {
 
-    public typealias UIViewControllerType = MermaidPreviewController
+    
+    var text: String?
+//    var options: Options
+    
+    public init(text: String? )
+    {
+        self.text = text
+    }
 
     public func makeUIViewController(context: Context) -> MermaidPreviewController {
         MermaidPreviewController()
     }
     
     public func updateUIViewController(_ uiViewController: MermaidPreviewController, context: Context) {
-        
+        if let text {
+            uiViewController.requestUpdate( text: text )
+        }
     }
     
     
--- MermaidPreview/Sources/MermaidPreview/MermaidPreviewController.swift
+++ MermaidPreview/Sources/MermaidPreview/MermaidPreviewController.swift
@@ -9,20 +9,58 @@ import Foundation
 import UIKit
 import WebKit
 
+fileprivate class ScriptQueue {
+    
+    var _scripts:Array<String> = []
+    
+    var elements:Array<String> {
+        get { _scripts }
+    }
+    
+    var isEmpty:Bool {
+        _scripts.isEmpty
+    }
+    
+    func push( _ item: String ) {
+        _scripts.append( item )
+    }
+    
+    func pop() -> String? {
+        guard  !_scripts.isEmpty else {
+            return nil
+        }
+        
+        return _scripts.removeLast()
+    }
+    
+    public func clear() {
+        _scripts.removeAll()
+    }
+    
+}
+
 public class MermaidPreviewController : UIViewController, WKUIDelegate, WKNavigationDelegate {
     var webView: WKWebView!
     
+    private var executionQueue = ScriptQueue()
+    
+    
     public override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
-        
 //        webConfiguration.userContentController.add(UpdateTextScriptHandler(self), name: "updateText")
         
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
         webView.navigationDelegate = self
         webView.backgroundColor = .none
+        if #available(iOS 16.4, *) {
+            webView.isInspectable = true
+        } 
+        /// [WKWebview Origin null is not allowed by Access-Control-Allow-Origin](https://stackoverflow.com/a/52634909/521197)
+        webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
         view = webView
     }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,9 +68,69 @@ public class MermaidPreviewController : UIViewController, WKUIDelegate, WKNaviga
     }
     
     private func loadWebAssets() {
-//        let myURL = Bundle.module.url(forResource: "index", withExtension: "html", subdirectory: "_Resources")
+        let myURL = Bundle.module.url(forResource: "index", withExtension: "html", subdirectory: "_Resources")
+        
 //        let myRequest = URLRequest(url: myURL!)
-//        webView.load(myRequest)
+        guard let myURL else {
+            fatalError( "index.html not found!")
+        }
+
+        webView.loadFileURL(myURL, allowingReadAccessTo: myURL.deletingLastPathComponent() )
+        
+    }
+    
+    // MARK: - WKWebView
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print( #function, "isLoading: \(webView.isLoading)" )
+
+        while( !executionQueue.isEmpty ) {
+            evaluateJavascript(executionQueue.pop()!)
+        }
+        
+    }
+    
+    public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
+        print( #function )
+    }
+            
+    func requestUpdate( text: String ) {
+        print( #function, "isLoading: \(webView.isLoading)" )
+
+        let javascript =
+"""
+document.getElementById('preview1')
+    .dispatchEvent( new CustomEvent( 'graph', { detail:`\(text)` } ) );
+"""
+        evaluateJavascript(javascript)
+
+    }
+    
+
+    private func evaluateJavascript(_ javascript: String) {
+
+        guard !webView.isLoading  else {
+            executionQueue.push( javascript )
+            return;
+        }
+        
+        webView.evaluateJavaScript(javascript, in: nil, in: WKContentWorld.page) {
+        result in
+          switch result {
+          case .failure(let error):        
+            var errorDescription = error.localizedDescription
+            if let err = error as NSError?, let desc = err.userInfo["WKJavaScriptExceptionMessage"] as? String {
+                errorDescription = desc
+            }
+            let alert = UIAlertController(title: "Error",
+                                          message: "Something went wrong while evaluating\n\(errorDescription)",
+                                          preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            break
+          case .success(_):
+            break
+          }
+        }
     }
 
 }
