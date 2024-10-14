1696c867c698d761e3db758088dd9ba556d0d0f5
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-10-14 19:08:58 +0200

    feat(commons): update SwiftyACE package

--- Commons/Package.swift
+++ Commons/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bsorrentino/SwiftyMonaco.git",  branch: "mermaid"),
-        .package(url: "https://github.com/bsorrentino/SwiftyACE.git", branch: "mermaid")
+        .package(url: "https://github.com/bsorrentino/SwiftyACE.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
